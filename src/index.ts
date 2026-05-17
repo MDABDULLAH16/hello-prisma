c3ccb49660ef7f6ea1c6a463695e5c76c97aed5a
MD ABDULLAH <mdabdullah161036@gmail.com>
2026-05-17 14:34:04 +0600

    crud operation recap

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,49 @@
 import { prisma } from "./lib/prisma.js";
 
 async function main() {
-    const user = await prisma.user.create({
-        data: {
-            email: 'abcd@gmail.com',
-            name: 'abdullah',
-            profilePhoto:'https://web.programming-hero/lv2.png'
-        }
-    })
-    console.log(user);
-    
+  // const user = await prisma.user.create({
+  //     data: {
+  //         email: 'asdf@gmail.com',
+  //         name: 'asdf',
+  //         profilePhoto:'https://web.programming-hero/lv2.png'
+  //     }
+  // })
+  // console.log(user);
+
+  // const updateOne = await prisma.user.update({
+  //     where: {
+  //         id:2
+  //     },
+  //     data: {
+  //         profilePhoto:'number 2'
+  //     }
+  // })
+  // console.log(updateOne);
+  // const updateMany = await prisma.user.updateMany({
+  //   where: {
+  //     profilePhoto: "number 2",
+  //   },
+  //   data: {
+  //     profilePhoto: "https//:web.programming-hero/level2.png",
+  //   },
+  // });
+  //       console.log(updateMany);
+
+//   const deleteOne = await prisma.user.delete({
+//     where: {
+//       id: 3,
+//     },
+//   });
+//   console.log(deleteOne);
+
+  const findUser = await prisma.user.findMany();
+  console.log(findUser);
+
+  // const findONe = await prisma.user.findUnique({
+  //     where: {
+  //         id:2
+  //     }
+  // })
+  // console.log(findONe);
 }
-main()
+main();
